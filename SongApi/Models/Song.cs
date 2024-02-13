225bee4524728550be6d1f23e5ef2a17277dd6b9
MajaAfzelius <majaafzelius01@gmail.com>
2024-02-13 10:30:48 +0100

    Moment4 VisualStudio

---
+++ SongApi/Models/Song.cs
@@ -0,0 +1,13 @@
+﻿namespace SongApi.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string artist { get; set; }
+        public string? album { get; set; }
+        public string title { get; set; }
+        public int length { get; set; }
+        public string category { get; set; }
+
+    }
+}
---
+++ SongApi/Models/SongContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SongApi.Models
+{
+    public class SongContext : DbContext
+    {
+        public SongContext(DbContextOptions<SongContext> options)
+            : base(options) 
+        { 
+        }
+        public DbSet<Song> Songs { get; set; } = null!;
+    }
+}
